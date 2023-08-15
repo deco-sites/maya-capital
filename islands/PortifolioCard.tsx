50603619ad139f411f47d5dc670b6fc5eac1a32a
KeplerFall <keplermpereira@gmail.com>
2023-08-14 22:07:16 -0300

    Portifolio

--- islands/PortifolioCard.tsx
+++ islands/PortifolioCard.tsx
@@ -13,8 +13,6 @@ export interface CardInfo {
 }
 */
 
-
-
 export default function PortifolioCard(props: CardInfo) {
   const { image, description, founder, website, sector, coinvestors } = props;
   const [openDialog, setDialog] = useState<boolean>(false);
@@ -25,37 +23,93 @@ export default function PortifolioCard(props: CardInfo) {
 
   return (
     <div className={`relative `}>
-      <div /*Image container*/ onClick={() => {toggleVisibility();}}className={`px-[20px] py-[50px]`}>
+      <div /*Image container*/
+        onClick={() => {
+          toggleVisibility();
+        }}
+        className={`px-[20px] py-[50px]`}
+      >
         <Image src={image || ``} width={88} class={`h-auto`} />
       </div>
-      <div className={`${!openDialog ? `hidden ` : `lg:bg-[rgb(255,255,255,0.5)] fixed top-0 left-0 bg-white w-[100vw] h-[100vh] px-[35px] py-[135px] z-50 lg:flex lg:justify-center lg:items-center`}`}>
-        <div className={`lg:px-[60px] lg:py-[100px] lg:max-w-[800px] lg:bg-white lg:border-2 lg:border-[#f5f5f5] rounded-lg`}>
-          <div className={`cursor-pointer flex justify-between relative px-[15px] mb-[40px]`}>
+      <div
+        className={`${
+          !openDialog
+            ? `hidden `
+            : `lg:bg-[rgb(255,255,255,0.5)] fixed top-0 left-0 bg-white w-[100vw] h-[100vh] px-[35px] py-[135px] z-50 lg:flex lg:justify-center lg:items-center`
+        }`}
+      >
+        <div
+          className={`lg:px-[60px] lg:py-[100px] lg:max-w-[800px] lg:bg-white lg:border-2 lg:border-[#f5f5f5] rounded-lg`}
+        >
+          <div
+            className={`cursor-pointer flex justify-between relative px-[15px] mb-[40px]`}
+          >
             <Image src={image || ``} width={100} />
-            <Image className={`absolute top-[-45px] right-[0px]`} src={`https://uploads-ssl.webflow.com/5ca9b45ad636b789185d2595/5cab6e021aed369c9b738cb4_modal-close-button.png `} width={65} onClick={() => {toggleVisibility();}}/>
+            <Image
+              className={`absolute top-[-45px] right-[0px]`}
+              src={`https://uploads-ssl.webflow.com/5ca9b45ad636b789185d2595/5cab6e021aed369c9b738cb4_modal-close-button.png `}
+              width={65}
+              onClick={() => {
+                toggleVisibility();
+              }}
+            />
           </div>
-          <p className={`text-[14px] text-[#333] leading-[1.5em] font-["Posteramatext"] font-light mb-[30px]`}>
+          <p
+            className={`text-[14px] text-[#333] leading-[1.5em] font-["Posteramatext"] font-light mb-[30px]`}
+          >
             {description}
           </p>
 
           <div className={`mb-[20px]`}>
-            <p className={`text-[#002fa7] text-[18px] font-semibold font-["Posteramatext"]`}>Founder</p>
-            <p className={`text-[#333] text-[14px] font-["Posteramatext"] font-normal`}>{founder}</p>
+            <p
+              className={`text-[#002fa7] text-[18px] font-semibold font-["Posteramatext"]`}
+            >
+              Founder
+            </p>
+            <p
+              className={`text-[#333] text-[14px] font-["Posteramatext"] font-normal`}
+            >
+              {founder}
+            </p>
           </div>
 
           <div className={`mb-[20px]`}>
-            <p className={`text-[#002fa7] text-[18px] font-semibold font-["Posteramatext"]`}>Website</p>
-            <p className={`text-[#333] text-[14px] font-["Posteramatext"] font-normal`}><a href={`https://www.${website}`} target="_blank">{website}</a></p>
+            <p
+              className={`text-[#002fa7] text-[18px] font-semibold font-["Posteramatext"]`}
+            >
+              Website
+            </p>
+            <p
+              className={`text-[#333] text-[14px] font-["Posteramatext"] font-normal`}
+            >
+              <a href={`https://www.${website}`} target="_blank">{website}</a>
+            </p>
           </div>
 
           <div className={`mb-[20px]`}>
-            <p className={`text-[#002fa7] text-[18px] font-semibold font-["Posteramatext"]`}>Sector</p>
-            <p className={`text-[#333] text-[14px] font-["Posteramatext"] font-normal`}>{sector}</p>
+            <p
+              className={`text-[#002fa7] text-[18px] font-semibold font-["Posteramatext"]`}
+            >
+              Sector
+            </p>
+            <p
+              className={`text-[#333] text-[14px] font-["Posteramatext"] font-normal`}
+            >
+              {sector}
+            </p>
           </div>
 
           <div className={`mb-[20px]`}>
-            <p className={`text-[#002fa7] text-[18px] font-semibold font-["Posteramatext"]`}>Co-Investors</p>
-            <p className={`text-[#333] text-[14px] font-["Posteramatext"] font-normal`}>{coinvestors}</p>
+            <p
+              className={`text-[#002fa7] text-[18px] font-semibold font-["Posteramatext"]`}
+            >
+              Co-Investors
+            </p>
+            <p
+              className={`text-[#333] text-[14px] font-["Posteramatext"] font-normal`}
+            >
+              {coinvestors}
+            </p>
           </div>
         </div>
       </div>
--- sections/Portifolio.tsx
+++ sections/Portifolio.tsx
@@ -19,22 +19,41 @@ export default function Portifolio(props: Props) {
   const { subTitle, title, cards } = props;
 
   return (
-    <section className={`py-[120px] px-[30px] background-[#f9fafc] lg:px-[115px]`}>
-      <p className={`font-["Posteramatext"] text-[#002fa7] text-[10px] uppercase tracking-[4px] lg:text-[16px] lg:tracking-[6px]`}>
+    <section
+      className={`py-[120px] px-[30px] background-[#f9fafc] lg:px-[115px]`}
+    >
+      <p
+        className={`font-["Posteramatext"] text-[#002fa7] text-[10px] uppercase tracking-[4px] lg:text-[16px] lg:tracking-[6px]`}
+      >
         {subTitle}
       </p>
 
       <div className={`flex flex-row items-baseline mb-[20px]`}>
-        <p className={` ml-[-3px] font-["Posteramatext"] font-semibold text-[#002fa7] text-[36px] lg:text-[80px]`}>
+        <p
+          className={` ml-[-3px] font-["Posteramatext"] font-semibold text-[#002fa7] text-[36px] lg:text-[80px]`}
+        >
           {title}
         </p>
-        <p class={`text-[#feb13f] text-[36px] font-["arial"] font-bold lg:text-[80px]`}>.</p>
+        <p
+          class={`text-[#feb13f] text-[36px] font-["arial"] font-bold lg:text-[80px]`}
+        >
+          .
+        </p>
       </div>
 
-      <div className={`grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 lg:max-w-[1200px]`}>
+      <div
+        className={`grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 lg:max-w-[1200px]`}
+      >
         {cards?.map((item) => {
           return (
-            <PortifolioCard image={item?.image} description={item?.description} founder={item?.founder} website={item?.website} sector={item?.sector} coinvestors={item?.coinvestors} />
+            <PortifolioCard
+              image={item?.image}
+              description={item?.description}
+              founder={item?.founder}
+              website={item?.website}
+              sector={item?.sector}
+              coinvestors={item?.coinvestors}
+            />
           );
         })}
       </div>
