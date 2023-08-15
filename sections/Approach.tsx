9bd100b3c758ea637728c7ae7c90ee8a94bd04e7
KeplerFall <keplermpereira@gmail.com>
2023-08-15 10:42:52 -0300

    finished fixes

--- sections/Approach.tsx
+++ sections/Approach.tsx
@@ -11,7 +11,7 @@ export default function Approach(props: Props) {
 
   return (
     <section
-      className={`flex justify-center flex-col items-center px-[20px] py-[40px] bg-[#f9fafc] lg:py-[120px]`}
+      className={`flex justify-center flex-col items-center px-[20px] py-[40px] bg-[#f9fafc] lg:py-[120px] lg:mb-[30px]`}
     >
       <p
         className={`font-["Posteramatext"] text-[#002fa7] text-[10px] font-light uppercase tracking-[4px] lg:text-[16px] lg:tracking-[6px]`}
@@ -31,13 +31,13 @@ export default function Approach(props: Props) {
         </p>
       </div>
       <p
-        className={`text-center text-[#333] text-[14px] font-["Posteramatext"] mb-[45px] lg:max-w-[60ch]`}
+        className={`text-center text-[#333] text-[14px] font-["Posteramatext"] mb-[20px] lg:mb-[45px] lg:max-w-[60ch]`}
       >
         {description}
       </p>
 
       <div className={`grid grid-cols-3 gap-[15px] p-[15px]`}>
-        <div className={`flex justify-center mt-[30px] `}>
+        <div className={`flex justify-center mt-[30px] lg:mt-[5px] `}>
           <Image
             className={`lg:w-[130px]`}
             src={`https://uploads-ssl.webflow.com/5ca9b45ad636b789185d2595/5ca9d9f21aed36bc626e098a_turn-right-arrow-with-broken-line-1.svg`}
@@ -48,10 +48,10 @@ export default function Approach(props: Props) {
           <p
             className={`text-center text-[14px] font-light font-["Posteramatext"] text-[#333] lg:text-[30px]`}
           >
-            Invest in exeptional teams
+            Invest in exceptional teams
           </p>
         </div>
-        <div className={`flex justify-center mt-[30px]`}>
+        <div className={`flex justify-center mt-[30px] lg:mt-[5px]`}>
           <Image
             className={`lg:w-[130px]`}
             src={`https://uploads-ssl.webflow.com/5ca9b45ad636b789185d2595/5ca9d9f11aed36be986e0987_turn-right-arrow-with-broken-line.svg`}
@@ -59,21 +59,21 @@ export default function Approach(props: Props) {
           />
         </div>
 
-        <div className={`flex justify-center`}>
+        <div className={`flex justify-center lg:justify-left`}>
           <p
-            className={`text-left text-[14px] font-light font-["Posteramatext"] text-[#333] lg:text-[30px]`}
+            className={`text-left text-[14px] font-light font-["Posteramatext"] text-[#333] lg:text-[30px] lg:ml-[-100px]`}
           >
-            Inspire future generations
+            Inspire future generations  
           </p>
         </div>
         <div className={`flex justify-center`}>
           <Image
-            className={`lg:w-[130px]`}
+            className={`lg:w-[160px]`}
             src={`https://uploads-ssl.webflow.com/5ca9b45ad636b789185d2595/5ca9c09ff8e0125dc4e2977e_Maya%20Blue.svg`}
             width={115}
           />
         </div>
-        <div className={`flex justify-center`}>
+        <div className={`flex justify-center lg:justify-right`}>
           <p
             className={`text-right text-[14px] font-light font-["Posteramatext"] text-[#333] lg:text-[30px]`}
           >
@@ -103,6 +103,16 @@ export default function Approach(props: Props) {
           />
         </div>
       </div>
+
+      <a href={`https://www.maya.capital/approach`} className={`mt-[35px] lg:mt-[95px] flex gap-[20px] items-center`}><p className={`text-[#002fa7] tracking-[2px] cursor-pointer text-[12px] lg:text-[16px] font-semibold uppercase font-["Posteramatext"]`}>Learn More</p><div
+            className={` flex align-center justify-center bg-[#feb13f] w-[18px] h-[18px] rounded-full lg:w-[28px] lg:h-[28px] items-center`}
+          >
+            <Image
+              src={`https://uploads-ssl.webflow.com/5ca9b45ad636b789185d2595/5ca9b9427177dc81cad32c68_right-arrow.png`}
+              width={16}
+              className={`w-[16px] h-[9px] lg:w-[17px] lg:h-[10px]`}
+            />
+          </div></a>
     </section>
   );
 }
